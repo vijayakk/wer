247982653e48cf6830e9730b2a8cd06e1241d7f0
vijayakk <48269086+vijayakk@users.noreply.github.com>
2019-03-13 23:07:23 +0530

    ff.java
    1st

--- ff.java
+++ ff.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
+      // Prints "Hello, World" in the terminal windowy.
       System.out.println("Hello, World");
    }
 }
